8eea6307dee71f4c01dfa7c9dd734c32d8b710c7
C-Dubb <gooberguy6@gmail.com>
2015-02-25 20:24:50 -0800

    Added small changes to particles
    Added the ability to toggle between clicks and mouse movement for
    spawning of particles

--- Particle System/example.py
+++ Particle System/example.py
@@ -15,8 +15,11 @@ class Example():
 		self.FPS = 50
 		self.screen = pygame.display.set_mode(self.res)
 
+		self.font = pygame.font.SysFont("Calibri", 16)
+
 		self.pManager = ParticleManager()
 
+		self.constant = False
 		self.running = True
 
 		while self.running:
@@ -36,15 +39,37 @@ class Example():
 			if event.type == QUIT:
 				self.running = 0
 				pygame.quit()
-			elif event.type == MOUSEBUTTONDOWN:
-				print self.getColor()
+			elif event.type == KEYDOWN:
+				if event.key == 32:
+					if self.constant:
+						self.constant = False
+					else:
+						self.constant = True
+
+			elif event.type == MOUSEBUTTONDOWN and not self.constant:
 				pos = pygame.mouse.get_pos()
 				shape = choice(['circle', 'rect'])
-				print shape
 				self.pManager.new(pos, size=randint(4,7), density=randint(4,7), color=self.getColor(), shape=shape)
+
+			elif event.type == MOUSEMOTION and self.constant:
+				pos = pygame.mouse.get_pos()
+				shape = choice(['circle', 'rect'])
+				self.pManager.new(pos, size=randint(4,7), density=randint(4,7), color=self.getColor(), shape=shape)
+
+	def blitText(self):
+		text = self.font.render("Constant: " + str(self.constant), 1, (255,255,255))
+		text2 = self.font.render("(Space to toggle)", 1, (255,255,255))
+		textrect, text2rect = text.get_rect(), text2.get_rect()
+		
+		textrect.centerx = text2rect.centerx = self.res[0]/2
+		text2rect.centery, textrect.centery = 38, 20
+
+		self.screen.blit(text, textrect)
+		self.screen.blit(text2, text2rect)
 
 	def Update(self):
 		self.screen.fill(0)
+		self.blitText()
 		self.pManager.update(self.screen)
 
 Example()
